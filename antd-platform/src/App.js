98ae7cf429c47ca5bf85606fa6dc67703994f963
slm <slm@imlaidian.com>
2018-11-23 18:42:03 +0800

    antd platfrom 项目搭建

---
+++ antd-platform/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Button } from 'antd';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button type="primary">按钮</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
